3ed504e13cb7a08dfcc2d3223d507e505ebedab7
160924.work <160924.work@gmail.com>
2024-12-08 05:29:09 +0000

    first commit

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -5,8 +5,8 @@ import User from "@models/user";
 const handler = NextAuth({
     providers:[
         GoogleProvider({
-            clientId:"483939972261-1eirc63n3nnidbiad9k6jeo72af142p4.apps.googleusercontent.com",
-            clientSecret:"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"
+            clientId:process.env.GOOGLE_ID,
+            clientSecret:process.env.GOOGLE_CLIENT_SECRET
         })
     ],
     async session({session}){
